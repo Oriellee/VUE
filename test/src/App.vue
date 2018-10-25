3e877527bd967f0c782ea07ebd8e8d53b84b6325
Oriel <1102386151@qq.com>
2018-10-25 18:57:52 +0800

    new

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <div>hahahhahaha</div>
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        哈哈哈哈哈哈哈哈哈哈
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
---
+++ vue_ele/src/components/foo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>foo34543534535345345</div>
+</template>
+
+<script>
+    export default {
+        name: "foo"
+    }
+</script>
+
+<style scoped>
+
+</style>
