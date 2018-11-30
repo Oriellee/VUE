e433d2e85db6ac90461f602b1dc47a60fac82a50
Oriel <1102386151@qq.com>
2018-11-30 18:30:35 +0800

    new

--- simpleframe/frontend/src/static/axios/index.js
+++ simpleframe/frontend/src/static/axios/index.js
@@ -3,14 +3,29 @@ import {Message, MessageBox} from 'element-ui'
 import store from '../store/index'
 import router from '../router/index'
 
-let cancel, promiseArr = {};
+let cancel = {};
 const CancelToken = axios.CancelToken;
+let pending = []; //声明一个数组用于存储每个ajax请求的取消函数和ajax标识
 
+let removePending = (config) => {
+    for (let p in pending) {
+        if (pending[p].u === config.url + '&' + config.method) { //当当前请求在数组中存在时执行函数体
+            pending[p].f(); //执行取消操作
+            pending.splice(p, 1); //把这条记录从数组中移除
+        }
+    }
+};
 axios.defaults.headers['sctoken'] = store.state.sctoken;
 axios.defaults.timeout = 5000;
 
 // 请求拦截器。
 axios.interceptors.request.use(config => {
+    removePending(config); //在一个ajax发送前执行一下取消操作
+    config.cancelToken = new CancelToken((c) => {
+        // 这里的ajax标识我是用请求地址&请求方式拼接的字符串，当然你可以选择其他的一些方式
+        pending.push({u: config.url + '&' + config.method, f: c});
+    });
+
     if (store.state.sctoken) {
         config.headers.sctoken = store.state.sctoken;
     }
@@ -21,6 +36,7 @@ axios.interceptors.request.use(config => {
 
 // 响应拦截器和异常处理。
 axios.interceptors.response.use(response => {
+    removePending(response.config);
     return response;
 }, error => {
     let errorText = "";
@@ -96,7 +112,7 @@ export default {
                 method: 'get',
                 url,
                 params: param,
-                concelToken: new CancelToken(c => {
+                cancelToken: new CancelToken(c => {
                     cancel = c;
                 })
             }).then(res => {
