f19287e24d6715794a282f97b6d7f810274b486d
Oriel <1102386151@qq.com>
2019-02-27 18:33:49 +0800

    new

--- simpleframe/frontend/vue.config.js
+++ simpleframe/frontend/vue.config.js
@@ -4,11 +4,11 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://127.0.0.1:8000/api',
-                changeOrigin: true,
-                ws: true,
+                target: 'http://127.0.0.1:8000/api',   // 需要请求的地址
+                changeOrigin: true,  // 是否跨域
+                ws: true,                         // proxy websockets 加上这一句试试看
                 pathRewrite: {
-                    '^/api': ''
+                    '^/api': ''  // 替换target中的请求地址，也就是说，在请求的时候，url用'/proxy'代替'http://ip.taobao.com'
                 }
             }
         },
